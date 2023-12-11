943411508c1d5a16990ca0608f0a8c2d22e91532
Faith Collison <faithcollison@yahoo.co.uk>
2023-12-11 15:19:57 +0000

    add single article page linked from article list

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import ArticleList from './components/ArticleList'
+import SingleArticle from './components/SingleArticle'
 import Header from './components/Header'
 import Nav from './components/Nav'
 import Home from './components/home'
@@ -13,6 +14,7 @@ function App() {
         <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/articles" element={<ArticleList />}/>
+            <Route path="/articles/:article_id" element={<SingleArticle />} />
         </Routes>
         
         
