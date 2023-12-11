63ffc3dae7bda336ac86b3bc1c88a1355a2b21a3
Faith Collison <faithcollison@yahoo.co.uk>
2023-12-11 16:51:59 +0000

    add in single article

---
+++ src/components/SingleArticle.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { getArticleById } from "../api"
+
+const SingleArticle = () => {
+    const [article, setArticle] = useState({})
+    const {article_id} = useParams()
+    
+    useEffect(() => {
+        getArticleById(article_id)
+        .then((data) => {
+            setArticle(data.article)
+        })
+    }, [])
+    return (
+        <div className="single-article-container">
+            <h2 className="single-article-text"> {article.title} </h2>
+            <p className="single-article-text"> Written by: {article.author} </p>
+            <p className="single-article-text"> {article.body} </p>
+            <img className="single-article-img" src={article.article_img_url} />
+            <p> Votes: {article.votes} </p>
+            <p> Comments: {article.comment_count} </p>
+        </div>
+    )
+}
+export default SingleArticle
