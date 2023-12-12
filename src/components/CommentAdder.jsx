5da322b4542bda0dbad787fe2b5a18043f771e1d
Faith Collison <faithcollison@yahoo.co.uk>
2023-12-12 16:01:35 +0000

    complete add comment box and alerts to user made

--- src/components/CommentAdder.jsx
+++ src/components/CommentAdder.jsx
@@ -3,28 +3,53 @@ import { postComment } from "../api"
 
 const CommentAdder = ({setComments, article_id}) => {
     const [newComment, setNewComment] = useState("")
+    const [success, setSuccess] = useState(undefined)
+    const [fail, setFail] = useState(undefined)
+    const [isDisabled, setIsDisabled] = useState(false)
+
+    
 
     const handleSubmit = (event) => {
+    if(newComment.length > 0){
         event.preventDefault();
+        setFail(false)
+        setSuccess(true)
+        setIsDisabled(true)
+        setTimeout(() => {
+            setSuccess(false);
+            setIsDisabled(false);
+            }, 1000);
         postComment(article_id, newComment)
         .then((newCommentFromApi) => {
+            setIsDisabled(false)
             setNewComment("")
             setComments((currComments) => {
                 return [newCommentFromApi, ...currComments]
             })
         })
+        .catch((err) => {
+            setSuccess(false)
+        })
+    }
+    else {
+        event.preventDefault();
+        setFail(true)
+    }
     }
 
     return (
         <div className="comment-adder">
         <form  onSubmit={handleSubmit}>
             <label htmlFor="newComment"> Add a comment </label>
-            <textarea className="comment-text-box" id="newComment" multiline="true" value={newComment} onChange={(event) => setNewComment(event.target.value)}></textarea>
-            <button className="comment-adder-button">
+            <textarea className="comment-text-box" id="newComment" multiline="true" value={newComment} onChange={(event) => setNewComment(event.target.value)}> </textarea>
+            <button className="comment-adder-button" disabled={isDisabled}>
                 Add
             </button>
+            { success ? "✅" : ""}
+            { fail ? "Please try again" : ""}
         </form>
         </div>
     )
 }
+
 export default CommentAdder;
--- src/components/CommentItem.jsx
+++ src/components/CommentItem.jsx
@@ -1,7 +1,6 @@
 const CommentItem = ({comment}) => {
     return (
         <div>
-            <h3> Comment: </h3>
             <p className="comment-text"> {comment.body} </p>
             <p> Written by: {comment.author} </p>
             <p> {comment.votes} Votes </p>
